f612b89947a98c5b0bc79b31e200c7c41986fa38
Kamden Brothers <kamdenbrothers@gmail.com>
2024-10-07 12:24:22 -0600

    Change code to jquery

--- static/JavaScript/Inventory.js
+++ static/JavaScript/Inventory.js
@@ -1,5 +1,4 @@
-
-let default_image = "static\\Resources\\CarLot.jpg"
+const default_image = "static/Resources/no_image.jpg"; // Updated to single default image
 
 // Store data for a vehicle
 class Vehicle {
@@ -14,213 +13,161 @@ class Vehicle {
         this.miles = miles.replace(/['"]+/g, '');
         this.color = color.replace(/['"]+/g, '');
         this.price = price.replace(/['"]+/g, '');
-		console.log(price)
-		console.log(imgs)
-		this.imgs = imgs//.replace(/['"]+/g, '');
-		if (this.imgs.length > 0) {
-			this.img_path = this.imgs[0].replace(/['"]+/g, '');
-		} else {
-			this.img_path = default_image;
-			console.log(default_image)
-		}
-        this.html = null
-        // string for searching
-        this.search_string = this.stockNumber + ' ' + this.year + ' ' + this.make + ' ' + this.model + ' ' + this.transmission + ' ' + this.vin + ' ' + this.color
+        this.imgs = imgs;
+
+        this.img_path = this.imgs.length > 0
+            ? "static/Images/" + this.imgs[0].replace(/['"]+/g, '')
+            : default_image;
+
+        this.html = null;
+        this.search_string = `${this.stockNumber} ${this.year} ${this.make} ${this.model} ${this.transmission} ${this.vin} ${this.color}`;
     }
 }
 
 // List of all vehicles
-var invVehicles = [];
+let invVehicles = [];
 
 // Tracks direction of arrow
-var asc_desc_sort = 1
+let asc_desc_sort = 1;
 
 // Add div with text to element
 function add_element(el, text) {
-    div = document.createElement('div');
-    div.innerHTML = text
-    el.appendChild(div)
+    $("<div></div>").text(text).appendTo(el);
 }
 
-// Sort vehicles based on and attribute (Year, Miles, or Price)
-function sort_objects(items, sort_attribute="Year") {
+// Sort vehicles based on an attribute (Year, Miles, or Price)
+function sort_objects(items, sort_attribute = "Year") {
     return items.sort((a, b) => {
-        console.log(sort_attribute)
-        if (sort_attribute == "Miles") {
+        let nameA, nameB;
+        if (sort_attribute === "Miles") {
             nameA = Number(a.miles);
             nameB = Number(b.miles);
-        }
-        else if (sort_attribute == "Price") {
+        } else if (sort_attribute === "Price") {
             nameA = Number(a.price);
             nameB = Number(b.price);
-        }
-        else {
+        } else {
             nameA = a.year + a.make.toUpperCase() + a.model.toUpperCase();
             nameB = b.year + b.make.toUpperCase() + b.model.toUpperCase();
         }
 
-        if (nameA < nameB) {
-            return -1 * asc_desc_sort;
-        }
-        if (nameA > nameB) {
-            return 1 * asc_desc_sort;
-        }
-
-        // names must be equal
-        return 0;
-    })
+        return (nameA < nameB ? -1 : (nameA > nameB ? 1 : 0)) * asc_desc_sort;
+    });
 }
 
 $.getJSON("/all_vehicles", function (result) {
-    console.log(result['all_vehicles'])
+    // Create list of all current vehicles
+    invVehicles = Object.entries(result['all_vehicles']).map(([key, currentLine]) =>
+        new Vehicle(key, ...currentLine)
+    );
 
-    var MappedVehicles = [];
-    MappedVehicles = Object.entries(result['all_vehicles']).map(([k, v]) => {
-        return [k, v]
-    });
-    MappedVehicles.forEach(function (item, index) {
-        var currentLine = item[1]
-        let p = new Vehicle(item[0], currentLine[0], currentLine[1], currentLine[2], currentLine[3], currentLine[4], currentLine[5], currentLine[6], currentLine[7], currentLine[8], currentLine[9])
-        invVehicles.push(p)
-    });
-    invVehicles = sort_objects(invVehicles)
-    console.log(invVehicles)
-    let item;
-    CarWrapper = document.getElementById("AllCars");
-    imageDirectory = "static\\Images\\";
+    // Sort vehicles
+    invVehicles = sort_objects(invVehicles);
 
-    table = document.createElement('table');
-    CarWrapper.appendChild(table)
+    const CarWrapper = $("#AllCars");
+    const table = $("<table></table>").appendTo(CarWrapper).attr('id', 'main_table');
 
     for (let i = 0; i < invVehicles.length; i++) {
-        if (i % 3 == 0) {
-            tr = document.createElement('tr')
-            table.appendChild(tr)
+        // Create a new row after appending 3 items
+        if (i % 3 === 0) {
+            const tr = $("<tr></tr>").appendTo(table);
         }
 
-        CurrentVehicle = invVehicles[i]
+        const CurrentVehicle = invVehicles[i];
 
         // Load car image details
-        let link = document.createElement('a');
-        link.href = 'ProductPage?stockNumber=' + CurrentVehicle.stockNumber;
-        let img = document.createElement('img');
-        console.log(invVehicles)
-        img.src = imageDirectory + invVehicles[i].img_path;
-        img.classList.add("InventoryImage")
+        const link = $("<a></a>").attr("href", 'ProductPage?stockNumber=' + CurrentVehicle.stockNumber);
+        const img = $("<img>").attr("src", CurrentVehicle.img_path).addClass("InventoryImage");
 
-        image_table_data = document.createElement('td');
-        image_table_data.classList.add('CarImage')
-        image_table_data.id = invVehicles[i].stockNumber;
+        const image_table_data = $("<td></td>").addClass('CarImage').attr('id', CurrentVehicle.stockNumber);
 
         // Add image to table
-        image_table_data.appendChild(link)
-        link.appendChild(img)
+        link.append(img);
+        image_table_data.append(link);
 
-        link = document.createElement('a');
-        let linkText = document.createTextNode(CurrentVehicle.year + " " + CurrentVehicle.make + ' ' + CurrentVehicle.model);
-        link.appendChild(linkText);
-        link.title = CurrentVehicle.make + ", " + CurrentVehicle.model;
-        link.href = 'ProductPage?stockNumber=' + CurrentVehicle.stockNumber;
+        // Create Heading with Car year make and model that links to product page
+        const linkText = `${CurrentVehicle.year} ${CurrentVehicle.make} ${CurrentVehicle.model}`;
+        const linkTextElement = $("<a></a>")
+            .text(linkText)
+            .attr("title", `${CurrentVehicle.make}, ${CurrentVehicle.model}`)
+            .attr("href", 'ProductPage?stockNumber=' + CurrentVehicle.stockNumber)
+            .addClass("Heading");
 
-        image_table_data.id = (CurrentVehicle.stockNumber + "button");
+        image_table_data.attr('id', CurrentVehicle.stockNumber + "button").append(linkTextElement);
+        add_element(image_table_data, "Miles: " + CurrentVehicle.miles);
+        add_element(image_table_data, "Price: " + CurrentVehicle.price);
 
-        table.appendChild(image_table_data)
-
-        link.allText = CurrentVehicle.make + ", " + CurrentVehicle.model
-        link.classList.add("Heading")
-
-        image_table_data.appendChild(link);
-        add_element(image_table_data, "Miles: " + CurrentVehicle.miles)
-        add_element(image_table_data, "Price: " + CurrentVehicle.price)
-
-        linebreak = document.createElement("br");
-        image_table_data.appendChild(linebreak);
-
-        tr.appendChild(image_table_data);
-        CurrentVehicle.html = image_table_data
+        image_table_data.append("<br>");
+        const tr = table.find('tr').last(); // Find the last row to append to
+        tr.append(image_table_data);
+        CurrentVehicle.html = image_table_data;
     }
+});
 
-})
-
-
+// Limit list based on SearchOption
 function search_cars() {
-    SearchBar = document.getElementById("SearchBar")
-    SearchOption = document.getElementById("SearchOption")
+    const SearchBar = $("#SearchBar");
+    const SearchOption = $("#SearchOption");
 
-    console.log(SearchBar)
-    console.log(SearchBar.value)
-    console.log(SearchOption.value)
+    // Functions for filtering items
+    const keyword = item => item.search_string.toLowerCase().includes(SearchBar.val().toLowerCase());
+    const min_miles = item => Number(item.miles) > Number(SearchBar.val().replace(/,/g, ''));
+    const max_miles = item => Number(item.miles) < Number(SearchBar.val().replace(/,/g, ''));
+    const min_price = item => Number(item.price) > Number(SearchBar.val().replace(/,/g, ''));
+    const max_price = item => Number(item.price) < Number(SearchBar.val().replace(/,/g, ''));
+    const vin = item => item.vin.toLowerCase().includes(SearchBar.val().toLowerCase());
 
-    function keyword(item) {
-        return item.search_string.toLowerCase().includes(SearchBar.value.toLowerCase())
-    }
-    function min_miles(item) {
-        console.log(Number(item.miles))
-        console.log(Number(SearchBar.value.replace(/,/g, '')))
-        console.log()
-        return Number(item.miles) > Number(SearchBar.value.replace(/,/g, ''))
-    }
-    function max_miles(item) {
-        return Number(item.miles) < Number(SearchBar.value.replace(/,/g, ''))
-    }
-    function min_price(item) {
-        return Number(item.price) > Number(SearchBar.value.replace(/,/g, ''))
-    }
-    function max_price(item) {
-        return Number(item.price) < Number(SearchBar.value.replace(/,/g, ''))
-    }
-    function vin(item) {
-        return item.vin.toLowerCase().includes(SearchBar.value.toLowerCase())
+    let filter_function;
+    switch (SearchOption.val()) {
+        case "Key Word": filter_function = keyword; break;
+        case "Minimum Miles": filter_function = min_miles; break;
+        case "Maximum Miles": filter_function = max_miles; break;
+        case "Minimum Price": filter_function = min_price; break;
+        case "Maximum Price": filter_function = max_price; break;
+        case "VIN": filter_function = vin; break;
     }
 
-    if (SearchOption.value == "Key Word") {filter_function = keyword}
-    else if (SearchOption.value == "Minimum Miles") {filter_function = min_miles}
-    else if (SearchOption.value == "Maximum Miles") {filter_function = max_miles}
-    else if (SearchOption.value == "Minimum Price") {filter_function = min_price}
-    else if (SearchOption.value == "Maximum Price") {filter_function = max_price}
-    else if (SearchOption.value == "VIN") {filter_function = vin}
+    // Delete previous data
+    $("table").remove();
+    const newTable = $("<table></table>").appendTo($("#AllCars")).attr('id', 'main_table');
 
-    table.remove()
-    table = document.createElement('table');
-    CarWrapper.appendChild(table)
-    
-    searchList = invVehicles.filter(filter_function)
-    i = 0
+    const searchList = invVehicles.filter(filter_function);
+    let i = 0;
+
+    let tr;
     searchList.forEach(car_obj => {
-        if (i % 3 == 0) {
-            tr = document.createElement('tr')
-            table.appendChild(tr)
+        // Create new row every 3
+        if (i % 3 === 0) {
+            tr = $("<tr></tr>").appendTo(newTable);
         }
 
-        tr.appendChild(car_obj.html);
-        i += 1;
-    })
+        // Add cars to row
+        tr.append(car_obj.html);
+        i++;
+    });
 }
 
-var SortVeh = document.getElementById('SortVehicles');
-SortVeh.onchange = (event) => {
-    var inputText = event.target.value;
-
-    invVehicles = sort_objects(invVehicles, inputText)
-    search_cars()
-}
+const SortVeh = $('#SortVehicles');
+SortVeh.change(event => {
+    const inputText = event.target.value;
+    invVehicles = sort_objects(invVehicles, inputText);
+    search_cars();
+});
 
 function flip_sort() {
-    SortButton = document.getElementById("SortButton")
-    if (SortButton.classList.value.includes("Rotate")) {
-        asc_desc_sort = 1
-        SortButton.classList.remove("Rotate")
+    const SortButton = $("#SortButton");
+    if (SortButton.hasClass("Rotate")) {
+        asc_desc_sort = 1;
+        SortButton.removeClass("Rotate");
     } else {
-        asc_desc_sort = -1
-        SortButton.classList.add("Rotate")
+        asc_desc_sort = -1;
+        SortButton.addClass("Rotate");
     }
-    inputText = SortVeh.value
-
-    invVehicles = sort_objects(invVehicles, inputText)
-    search_cars()
+    const inputText = SortVeh.val();
+    invVehicles = sort_objects(invVehicles, inputText);
+    search_cars();
 }
 
-var SearchOpt = document.getElementById('SearchOption');
-SearchOpt.onchange = (event) => {
-    search_cars()
-}
+const SearchOpt = $('#SearchOption');
+SearchOpt.change(() => {
+    search_cars();
+});
--- static/JavaScript/ProductPage.js
+++ static/JavaScript/ProductPage.js
@@ -1,108 +1,86 @@
 var urlParams = {};
 (window.onpopstate = function () {
+    // Get url parameters for current cars stock number
     var match,
-        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        pl = /\+/g,  // Regex for replacing addition symbol with a space
         search = /([^&=]+)=?([^&]*)/g,
         decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
-        query  = window.location.search.substring(1);
-  
+        query = window.location.search.substring(1);
+
     urlParams = {};
     while (match = search.exec(query))
-       urlParams[decode(match[1])] = decode(match[2]);
+        urlParams[decode(match[1])] = decode(match[2]);
 })();
 
-console.log(urlParams)
+console.log(urlParams);
 
 function add_break(element) {
-    linebreak = document.createElement("br");
-    element.appendChild(linebreak)
+    $('<br>').appendTo(element);
 }
 
 function add_element(el, text) {
-    div = document.createElement('div');
-    div.innerHTML = text
-    el.appendChild(div)
+    $('<div></div>').html(text).appendTo(el);
 }
 
-ImagePath = "static\\Images\\"
-default_image = "static\\Resources\\CarLot.jpg"
+const ImagePath = "static/Images/";
+const default_image = "static/Resources/CarLot.jpg";
 
 $.getJSON(`/get_vehicle_data?stockNumber=${urlParams.stockNumber}`, function (result) {
-    console.log(result)
-    product =  result.product
-    CarDiv = document.getElementById("Car Details");
-    if (!result.product) {
-        console.log('Could not find car with stock number')
-        let text = document.createElement('div');
-        text.classList.add("none_found")
-        text.innerHTML = "Could not find product"
+    const product = result.product;
+    // Get main div to insert data
+    const CarDiv = $("#CarDetails");
 
-        add_break(CarDiv)
-        CarDiv.appendChild(text)
-        add_break(CarDiv)
-        return
+    if (!product) {
+        // No product was found with id number
+        $('<br>').appendTo(CarDiv);
+        $('<div></div>').addClass("none_found").html("Could not find product").appendTo(CarDiv);
+        $('<br>').appendTo(CarDiv);
+        return;
     }
 
-    table = document.createElement("table");
-    CarDiv.appendChild(table)
+    const table = $("<table></table>").appendTo(CarDiv);
 
-    tr = document.createElement('tr')
-    table.appendChild(tr)
+    let tr = $("<tr></tr>").appendTo(table);
+    let td = $("<td></td>").appendTo(tr);
+    let img = $("<img>").addClass("display");
 
-    td = document.createElement('td')
-    tr.appendChild(td)
-    img = document.createElement('img')
-    img.classList.add("display")
-
-    images = product[9]
-    num_images = images.length
-    if (images == 0) {
-        img.src = default_image;
+    const images = product[9];
+    if (images.length === 0) {
+        // No image data. Add generic image
+        img.attr("src", default_image);
+    } else {
+        // Add first image
+        img.attr("src", ImagePath + images[0]);
     }
-    else {
-        img.src = ImagePath + images[0];
-    }
-    td.appendChild(img)
+    td.append(img);
 
-    td = document.createElement('td')
-    td.classList.add("information")
-    tr.appendChild(td)
+    // Create information table cell
+    td = $("<td></td>").addClass("information").appendTo(tr);
+    $("<h2></h2>").html(`${product[0]} ${product[1]} ${product[2]}`).appendTo(td);
 
-    h2 = document.createElement("h2")
-    h2.innerHTML = product[0] + " " + product[1] + " " + product[2]
-    td.appendChild(h2)
-
-    add_element(td, "Transmission: " + product[3])
-    add_element(td, "Engine: " + product[4])
-    add_element(td, "Miles: " + product[6])
-    add_element(td, "Color: " + product[7])
-    add_element(td, "Price: " + product[8])
-    add_element(td, "Vin: " + product[5])
-
+    // Add car information to page
+    $('<div></div>').html("Transmission: " + product[3]).appendTo(td);
+    $('<div></div>').html("Engine: " + product[4]).appendTo(td);
+    $('<div></div>').html("Miles: " + product[6]).appendTo(td);
+    $('<div></div>').html("Color: " + product[7]).appendTo(td);
+    $('<div></div>').html("Price: " + product[8]).appendTo(td);
+    $('<div></div>').html("Vin: " + product[5]).appendTo(td);
 
     for (let i = 1; i < images.length; i++) {
-        if (i % 2 == 1) {
-            tr = document.createElement('tr')
-            table.appendChild(tr)
+        if (i % 2 === 1) {
+            // Create new row every other image
+            tr = $("<tr></tr>").appendTo(table);
         }
 
-        td = document.createElement('td')
-        tr.appendChild(td)
-
-        img = document.createElement('img')
-        img.classList.add("display")
-        img.src = ImagePath + images[i];
-        td.appendChild(img)
+        td = $("<td></td>").appendTo(tr);
+        img = $("<img>").addClass("display").attr("src", ImagePath + images[i]);
+        td.append(img);
     }
 
-    console.log(images.length % 2)
-    if (images.length % 2 == 0 && images.length != 0) {
-        td = document.createElement('td')
-        tr.appendChild(td)
-
-        img = document.createElement('img')
-        img.classList.add("display")
-        img.src = default_image;
-        td.appendChild(img)
+    if (images.length % 2 === 0 && images.length !== 0) {
+        // Add default image if last image is on the left
+        td = $("<td></td>").appendTo(tr);
+        img = $("<img>").addClass("display").attr("src", default_image);
+        td.append(img);
     }
-})
+});
